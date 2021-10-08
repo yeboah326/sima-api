84ffccabd29f5e662b0ccfa5afa61820a65a0914
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-10-08 09:45:53 +0000

    Add code to create database

--- manage.py
+++ manage.py
@@ -1,8 +1,14 @@
 from flask.cli import FlaskGroup
 
-from api import app
+from api import app, db
 
 cli = FlaskGroup(app)
 
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 if __name__=="__main__":
+    recreate_db()
     cli()

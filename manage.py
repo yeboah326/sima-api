575f267c2ac01978a9bb115496efbf46cf7d207c
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-10-08 13:32:10 +0000

    Add create_db method

--- manage.py
+++ manage.py
@@ -4,11 +4,11 @@ from api import app, db
 
 cli = FlaskGroup(app)
 
+@cli.command("create_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
 
 if __name__=="__main__":
-    recreate_db()
     cli()
